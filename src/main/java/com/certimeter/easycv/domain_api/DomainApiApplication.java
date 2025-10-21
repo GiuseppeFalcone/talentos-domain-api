27fd8c38b8ab976bac6bb5586c98cb151e60540a
giuseppe.falcone <giuseppe.falcone@lutech.it>
2025-10-21 14:52:09 +0200

    init spring boot application

---
+++ src/main/java/com/certimeter/easycv/domain_api/DomainApiApplication.java
@@ -0,0 +1,13 @@
+package com.certimeter.easycv.domain_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DomainApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DomainApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/certimeter/easycv/domain_api/DomainApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.certimeter.easycv.domain_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DomainApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
